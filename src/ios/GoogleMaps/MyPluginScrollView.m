0894072be260223f4ee833e422adf011af9740dd
Johan Leuenberger <johan.leuenberger@vidinoti.com>
2018-03-09 16:19:09 +0100

    fix white bar at top of map with iOS 11

--- src/ios/GoogleMaps/MyPluginScrollView.m
+++ src/ios/GoogleMaps/MyPluginScrollView.m
@@ -16,6 +16,11 @@ UIView *myView = nil;
 {
   self = [super initWithFrame:aRect];
   self.debugView = [[MyPluginLayerDebugView alloc] initWithFrame:aRect];
+  // Avoid the white bar that appears at the top of the map with iPhone iOS 11
+  // See problem description here: https://github.com/mapsplugin/cordova-plugin-googlemaps/issues/1909
+  if (@available(iOS 11, *)) {
+    self.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+  }
   return self;
 }
 
